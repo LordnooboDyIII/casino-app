79a76a9c3d571e7a5678ddd18e6aa826170009c6
LordnooboDyIII <jodelbriones401@gmail.com>
2025-06-07 22:15:45 -0400

    added the forms on the contact page

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ Bootstrap
 */
 app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"));
 app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
+
+/*
+Middleware
+*/
 app.use(express.static(path.join(__dirname, "public")));
 
 /*
