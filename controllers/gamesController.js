6210b09b7f653fb89819497ebef6c744247d8bd5
ViswaBalaratnam <2230970@bdeb.qc.ca>
2025-06-18 19:44:18 -0400

    Ajout de session pour la page Dice

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -4,5 +4,7 @@ export const getDiceGamePage = asyncHandler(async (req, res) => {
   res.render("pages/games/dices", {
     siteTitle: "Casino App",
     pageTitle: "Dice Game",
+    user: req.session.user || null, // add this line
   });
+
 });
